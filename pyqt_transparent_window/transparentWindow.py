670e8d58b1340b7d8b2b8bab9686f912666cbc90
Jung Gyu Yoon <yjg30737@gmail.com>
2021-12-29 13:57:49 +0900

    Remove files for test

--- pyqt_transparent_window/transparentWindow.py
+++ pyqt_transparent_window/transparentWindow.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-from pyqt_transparent_window.resizeFrame import ResizeFrame
+from pyqt_resize_frame.resizeFrame import ResizeFrame
 
 
 class TransparentWindow(QWidget):
